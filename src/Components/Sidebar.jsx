8413a6bfff6d83ed7b20e78ead1bbc9381b3ebf6
David Moller <davidmollera@hotmail.com>
2023-07-13 11:34:09 -0500

    created a sidebar option element comopnent and created props for it

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../CSS/Sidebar.css'
 import { Twitter } from '@mui/icons-material';
+import SidebarOption from '../Elements/SidebarOption';
 
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             <Twitter/>
+
+            <SidebarOption icon='' title=''/>
         </div>
     );
 }
---
+++ src/Elements/SidebarOption.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SidebarOption = ({ icon, title }) => {
+    return (
+        <div className='sidebarOption'>
+            
+        </div>
+    );
+}
+
+export default SidebarOption;
