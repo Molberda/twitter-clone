a8862cb387a4fe53204a136510d4bbf6e27fe234
David Moller <davidmollera@hotmail.com>
2023-07-13 11:26:00 -0500

    created a sidebar component and sidebar css file, also installed the mui repository

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './CSS/App.css';
+import Sidebar from './Components/Sidebar.jsx';
 
 function App() {
   return (
     <div className="app">
       <h1>lets build a twitter clone</h1>
+      <Sidebar/>
     </div>
   );
 }
+
+// https://youtu.be/rJjaqSTzOxI?t=1613
 
 export default App;
---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../CSS/Sidebar.css'
+import { Twitter } from '@mui/icons-material';
+
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <Twitter/>
+        </div>
+    );
+}
+
+export default Sidebar;
