d524f78cd98733cde3909a465154c34e11018a9d
David Moller <davidmollera@hotmail.com>
2023-07-13 12:40:43 -0500

    started the styling for the sidebar option component

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import '../CSS/Sidebar.css'
-import { AccountCircleOutlined, BookmarkBorderOutlined, FormatListBulletedOutlined, Home, MailOutline, MoreHorizOutlined, NotificationsNone, Search, Twitter, VerifiedOutlined } from '@mui/icons-material';
+import { AccountCircleOutlined, BookmarkBorderOutlined,  Home, ListAltOutlined, MailOutline, MoreHorizOutlined, NotificationsNone, Search, Twitter, VerifiedOutlined } from '@mui/icons-material';
 import SidebarOption from '../Elements/SidebarOption';
+import { Button } from '@mui/material';
 
 
 const Sidebar = () => {
@@ -13,11 +14,13 @@ const Sidebar = () => {
             <SidebarOption Icon={<Search/>} title='Explore'/>
             <SidebarOption Icon={<NotificationsNone/>} title='Notifications'/>
             <SidebarOption Icon={<MailOutline/>} title='Messages'/>
-            <SidebarOption Icon={<FormatListBulletedOutlined/>} title='Lists'/>
+            <SidebarOption Icon={<ListAltOutlined/>} title='Lists'/>
             <SidebarOption Icon={<BookmarkBorderOutlined/>} title='Bookmarks'/>
             <SidebarOption Icon={<VerifiedOutlined/>} title='Verified'/>
             <SidebarOption Icon={<AccountCircleOutlined/>} title='Profile'/>
             <SidebarOption Icon={<MoreHorizOutlined/>} title='More'/>
+
+            <Button variant='contained'><h2>Tweet</h2></Button>
         </div>
     );
 }
--- src/Elements/SidebarOption.jsx
+++ src/Elements/SidebarOption.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../CSS/SidebarOption.css'
+import React from "react";
+import "../CSS/SidebarOption.css";
 
 const SidebarOption = ({ Icon, title }) => {
-    return (
-        <div className='sidebarOption'>
-            {Icon}
-            <h2>{title}</h2>
-        </div>
-    );
-}
+  return (
+    <div className="sidebarOption">
+      {Icon}
+      <h2>{title}</h2>
+    </div>
+  );
+};
 
 export default SidebarOption;
