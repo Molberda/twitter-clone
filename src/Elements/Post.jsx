f7028a1d2000d33fbc45cea2b5b052b35a532578
David Moller <davidmollera@hotmail.com>
2023-07-14 15:07:47 -0500

    used an avatar component for the post file

--- src/Elements/Post.jsx
+++ src/Elements/Post.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "../CSS/Post.css";
+import { Avatar } from "@mui/material";
 
 const Post = ({ displayName, username, verified, text, img, avatar }) => {
   return <div className="post">
-    
+    <div className="post__avatar">
+      <Avatar src='https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltee886584fd1cd345/60db8718892a730f5883a347/68a712aa7ffd3da7d91c2fe1469bda58990f7ab7.jpg?auto=webp&format=pjpg&width=3840&quality=60'/>
+    </div>
   </div>;
 };
 
