8a879ff95e0931566a62314d4fd8f9a4fe004f6e
David Moller <davidmollera@hotmail.com>
2023-07-17 14:26:01 -0500

    added twitter post and timeline to the code using twitter embed

--- src/Components/Widgets.jsx
+++ src/Components/Widgets.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import '../CSS/Widgets.css'
-import { Search } from '@mui/icons-material';
+import React from "react";
+import "../CSS/Widgets.css";
+import { Search } from "@mui/icons-material";
+import { TwitterTweetEmbed, TwitterTimelineEmbed } from "react-twitter-embed";
 
 const Widgets = () => {
-    return (
-        <div className='widgets'>
-            <div className="widgets__input">
-                <Search className='widgets__searchIcon'/> 
-                <input placeholder='Search Twitter' type="text" />
-            </div>
-            <div className="widgets__widgetContainer">
-                <h2>Whats Happening</h2>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="widgets">
+      <div className="widgets__input">
+        <Search className="widgets__searchIcon" />
+        <input placeholder="Search Twitter" type="text" />
+      </div>
+      <div className="widgets__widgetContainer">
+        <h2>Whats Happening</h2>
+
+        <TwitterTweetEmbed tweetId={"1680280727395483652"} />
+
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="Cr7stianismo_"
+          options={{ height: 400 }}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Widgets;
