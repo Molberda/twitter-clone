b8e2007155e766a1c98ff84e7c1ccf90cebd32cd
David Moller <davidmollera@hotmail.com>
2023-07-17 17:02:39 -0500

    added css for the widget input div and the input itself

--- src/Components/Widgets.jsx
+++ src/Components/Widgets.jsx
@@ -11,8 +11,8 @@ const Widgets = () => {
   return (
     <div className="widgets">
       <div className="widgets__input">
-        <Search className="widgets__searchIcon" />
         <input placeholder="Search Twitter" type="text" />
+        <Search className="widgets__searchIcon" />
       </div>
       <div className="widgets__widgetContainer">
         <h2>Whats Happening</h2>
@@ -25,7 +25,7 @@ const Widgets = () => {
           options={{ height: 400 }}
         />
 
-        <TwitterShareButton url="" options={{ text: "reactjs is awesome" }} />
+        <TwitterShareButton url={'https://google.com'} options={{ text: "reactjs is awesome", via: 'Molberda' }} />
       </div>
     </div>
   );
