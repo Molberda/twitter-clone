a72ef9db2f5190a6f699fa6de625bb9991873d81
David Moller <davidmollera@hotmail.com>
2023-07-20 10:04:42 -0500

    created firebase file and used the config data

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlFlUxdZ7brnzWHmzDMgNmPC2mFIzEErY",
+    authDomain: "dm-twitter-clone.firebaseapp.com",
+    projectId: "dm-twitter-clone",
+    storageBucket: "dm-twitter-clone.appspot.com",
+    messagingSenderId: "868375687909",
+    appId: "1:868375687909:web:81899163c977955af1da0c"
+  };
