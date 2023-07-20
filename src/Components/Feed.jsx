8bf3e13a8f1a8f7f9578c52d617998943eaf6f44
David Moller <davidmollera@hotmail.com>
2023-07-20 10:18:24 -0500

    added the props for the post component in feed component

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -13,7 +13,14 @@ const Feed = () => {
       </div>
 
       <Tweetbox />
-      <Post displayName="" username="" text="" img="" avatar="" verified />
+      <Post
+        displayName="David Moller"
+        username="molberda14"
+        text="this is a test"
+        img="Y2lkPTc5MGI3NjExYjUyNmJnd2RoOGQ5a2pyMjB5MGhqNHhhN2N5dDdhdmZodzVhOWJjMCZlcD12MV9naWZzX3NlYXJjaCZjdD1n"
+        avatar="https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltee886584fd1cd345/60db8718892a730f5883a347/68a712aa7ffd3da7d91c2fe1469bda58990f7ab7.jpg?auto=webp&format=pjpg&width=3840&quality=60"
+        verified={true}
+      />
       <Post displayName="" username="" text="" img="" avatar="" verified />
       <Post displayName="" username="" text="" img="" avatar="" verified />
     </div>
